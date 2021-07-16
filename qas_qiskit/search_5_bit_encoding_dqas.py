5a37c69ad542c3b88fa1c7690bab04bc77ef1e92
peiyong-addwater <peiyoingw@student.unimelb.edu.au>
2021-07-16 21:32:09 +1000

    update

--- qas_qiskit/search_5_bit_encoding_dqas.py
+++ qas_qiskit/search_5_bit_encoding_dqas.py
@@ -2,6 +2,7 @@ import numpy as np
 import jax.numpy as jnp
 import jax
 import time
+import json
 import optax
 from typing import (
     List,
@@ -34,7 +35,22 @@ from dqas_qiskit.standard_ops import (
 )
 from dqas_qiskit.search import train_circuit, dqas_qiskit
 
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NpEncoder, self).default(obj)
 
+def nowtime():
+    return str(time.strftime("%Y%m%d-%H%M", time.localtime()))
+
+file_name = nowtime()+"_FIVE_BIT_CODE_SEARCH.json"
+res_dict = {}
 
 line_five_qubits_connection = [(0,1),(1,0),(1,2),(2,1),(2,3),(3,2),(3,4),(4,3)]
 single_qubit_gate = ["U3Gate"]
@@ -43,10 +59,12 @@ two_qubit_gate = ["CU3Gate"]
 
 #pool =default_complete_graph_parameterized_pool(5)
 pool = GatePool(5, single_qubit_gate, two_qubit_gate, False, line_five_qubits_connection)
+res_dict["Pool"] = str(pool)
 print(pool)
 p = 30
 c = len(pool)
 l = 3
+res_dict["Search_Param"] = {"p":p, "c":c, "l":l}
 param = np.random.randn(p*c*l).reshape((p,c,l))
 a = np.zeros(p*c).reshape((p,c))
 final_prob_param, final_circ_param, final_prob_model, final_circ, final_k, final_op_list, final_loss, loss_list_qas= dqas_qiskit(
@@ -54,6 +72,15 @@ final_prob_param, final_circ_param, final_prob_model, final_circ, final_k, final
     IndependentCategoricalProbabilisticModel,
     prob_train_k_num_samples=300, verbose=2,train_circ_in_between_epochs=10,parameterized_circuit=True
 )
+res_dict["k"] = final_k
+res_dict["op_list"] = final_op_list
+res_dict["loss_list"] = loss_list_qas
+res_dict["prob_param"] = final_prob_param
+
+with open(file_name, 'w') as f:
+    json.dump(res_dict, f, indent=4, cls=NpEncoder)
+
+
 
 """
 # Fine tune the circuit after architecture search
