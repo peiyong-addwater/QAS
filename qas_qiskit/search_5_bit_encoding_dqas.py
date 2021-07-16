10340a945ecd0d054d781aad50efc3944fcc08ca
peiyong-addwater <peiyoingw@student.unimelb.edu.au>
2021-07-16 22:17:47 +1000

    update

--- qas_qiskit/search_5_bit_encoding_dqas.py
+++ qas_qiskit/search_5_bit_encoding_dqas.py
@@ -59,18 +59,22 @@ two_qubit_gate = ["CU3Gate"]
 
 #pool =default_complete_graph_parameterized_pool(5)
 pool = GatePool(5, single_qubit_gate, two_qubit_gate, False, line_five_qubits_connection)
+
 res_dict["Pool"] = str(pool)
+
 print(pool)
 p = 30
 c = len(pool)
 l = 3
+
 res_dict["Search_Param"] = {"p":p, "c":c, "l":l}
+
 param = np.random.randn(p*c*l).reshape((p,c,l))
 a = np.zeros(p*c).reshape((p,c))
 final_prob_param, final_circ_param, final_prob_model, final_circ, final_k, final_op_list, final_loss, loss_list_qas= dqas_qiskit(
     500, SIMPLE_DATASET_FIVE_BIT_CODE, a, param, pool, FiveBitCodeSearchDensityMatrixNoiseless,
     IndependentCategoricalProbabilisticModel,
-    prob_train_k_num_samples=300, verbose=2,train_circ_in_between_epochs=10,parameterized_circuit=True
+    prob_train_k_num_samples=300, verbose=2,train_circ_in_between_epochs=20,parameterized_circuit=True
 )
 res_dict["k"] = final_k
 res_dict["op_list"] = final_op_list
