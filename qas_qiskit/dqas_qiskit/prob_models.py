b0d30abd90c9037281c702944e1d6764ba8b8b9c
peiyong-addwater <peiyoingw@student.unimelb.edu.au>
2021-07-23 17:38:19 +1000

    update

--- qas_qiskit/dqas_qiskit/prob_models.py
+++ qas_qiskit/dqas_qiskit/prob_models.py
@@ -87,7 +87,7 @@ class IndependentCategoricalProbabilisticModel(ProbModelBaseClass):
     def sample_k(self, size):
         seeds = list(range(size))
         sampled_k_list = Parallel(n_jobs=-1, verbose=0)(delayed(categorical_sample)
-            (self.alpha, jax.random.PRNGKey(seed+random.randint(a=1000*size, b=1000000000*size))) for seed in seeds)
+            (self.alpha, jax.random.PRNGKey(seed)) for seed in seeds)
         return sampled_k_list
 
 
