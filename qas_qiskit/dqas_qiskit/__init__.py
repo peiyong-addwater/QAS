29aacc71d29288ee0e23a2b4a692e1e08872459a
peiyong-addwater <peiyoingw@student.unimelb.edu.au>
2021-07-25 09:29:59 +1000

    update

--- qas_qiskit/dqas_qiskit/__init__.py
+++ qas_qiskit/dqas_qiskit/__init__.py
@@ -2,8 +2,7 @@ __version__ = "0.0.1"
 __author__ = "Peiyong Wang"
 
 import numpy
-numpy.core.arrayprint._line_width = 80*2
-numpy.set_printoptions(precision=4)
+numpy.set_printoptions(precision=4,linewidth=1000)
 
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
