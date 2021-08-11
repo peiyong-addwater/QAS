c325eaff371a95b1a3dc761f47669d1355d25f79
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2021-08-12 01:43:33 +1000

    update

---
+++ qas_qiskit/cmab_qas/search.py
@@ -0,0 +1,98 @@
+import itertools
+import time
+import optax
+from joblib import Parallel, delayed
+from pprint import pprint
+from typing import (
+    List,
+    Sequence,
+    Any,
+    Tuple,
+    Callable,
+    Iterator,
+    Optional,
+    Union,
+    Iterable,
+    Dict,
+    AnyStr,
+    Set
+)
+from .standard_ops import GatePool, default_complete_graph_parameterized_pool, default_complete_graph_non_parameterized_pool
+from qiskit.quantum_info import DensityMatrix
+from .tree_qas import MCTSController, QASState, TreeNode
+import numpy as np
+import jax
+import jax.numpy as jnp
+
+
+def _circ_obj_get_loss_dm(circ_obj, circ_params, init_state, target_state:List[DensityMatrix]):
+    return circ_obj.get_loss(circ_params, init_state, target_state)
+
+def _circ_obj_get_gradient_dm(circ_obj, circ_params, init_state, target_state:List[DensityMatrix]):
+    return circ_obj.get_gradient(circ_params, init_state, target_state)
+
+
+def searchNonParameterized(
+        model,
+        data:List[List],
+        init_qubit_with_actions: Set,
+        num_iterations = 500,
+        num_warmup_iterations = 20,
+        arc_batchsize = 20,
+        alpha=1 / np.sqrt(2),
+        prune_constant=0.9,
+        eval_expansion_factor=100,
+        op_pool: GatePool = None,
+        target_circuit_depth=20,
+        first_policy='local_optimal',
+        second_policy='local_optimal'
+):
+    p = target_circuit_depth
+    l = 3
+    c = len(op_pool)
+    assert len(data) == 2
+    placeholder_params = np.zeros((p,c,l))
+    assert num_warmup_iterations<num_iterations//2
+    assert num_warmup_iterations >= 10
+    controller = MCTSController(model,
+                                data,
+                                iteration_limit=5,
+                                alpha=alpha,
+                                prune_constant=prune_constant,
+                                eval_expansion_factor=eval_expansion_factor,
+                                op_pool=op_pool,
+                                init_qubit_with_actions=init_qubit_with_actions,
+                                target_circuit_depth=target_circuit_depth,
+                                first_policy=first_policy,
+                                second_policy=second_policy)
+    current_best_arc = None
+    current_best_node = None
+    current_best_reward = None
+
+    for epoch in range(num_iterations):
+        arcs, nodes = [], []
+        if epoch<num_warmup_iterations:
+            print("="*10+"Warming at Epoch {}".format(epoch+1)+"="*10)
+            for _ in range(arc_batchsize):
+                k, node = controller.randomSample()
+                arcs.append(k)
+                nodes.append(node)
+        else:
+            print("=" * 10 + "Searching at Epoch {}".format(epoch + 1) + "=" * 10)
+            for _ in range(arc_batchsize):
+                k, node = controller.sampleArc()
+                arcs.append(k)
+                nodes.append(node)
+        for k, node in zip(arcs, nodes):
+            reward = controller.simulation(k, placeholder_params)
+            controller.backPropagate(node, reward)
+        current_best_arc, current_best_node = controller.exploitArc()
+        current_best_reward = controller.simulation(current_best_arc, placeholder_params)
+        print("Current Best Reward: {}".format(current_best_reward))
+        print("Current Best k:\n", current_best_arc)
+        print("Current Ops:")
+        print(current_best_node.state)
+
+    return current_best_arc, current_best_node, current_best_reward
+
+
---
+++ qas_qiskit/search_sth_tree_search.py
@@ -0,0 +1,13 @@
+from cmab_qas.search import searchNonParameterized
+from cmab_qas.standard_ops import default_complete_graph_non_parameterized_pool
+from cmab_qas.circuits import (
+    BitFlipSearchDensityMatrixNoiseless,
+    SIMPLE_DATASET_BIT_FLIP,
+    PhaseFlipDensityMatrixNoiseless,
+    SIMPLE_DATASET_PHASE_FLIP,
+    FourTwoTwoDetectionDensityMatrixNoiseless,
+    FOUR_TWO_TWO_DETECTION_CODE_DATA,
+    FiveBitCodeSearchDensityMatrixNoiseless,
+    SIMPLE_DATASET_FIVE_BIT_CODE
+)
+
