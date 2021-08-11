c3ffe71bf1b7c8d036763f685b0a5ffa1d286557
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2021-08-11 17:08:18 +1000

    update

--- qas_qiskit/cmab_qas/tree_qas.py
+++ qas_qiskit/cmab_qas/tree_qas.py
@@ -1,6 +1,7 @@
 from .standard_ops import GatePool
 from .standard_ops import QuantumGate
 from .circuits import QuantumCircuit
+from copy import deepcopy
 from typing import (
     List,
     Sequence,
@@ -30,7 +31,12 @@ class QASState():
 
     def getLegalActions(self):
         if self.current_depth == self.max_depth:
-            return None
+            return []
+        actions = []
+        for key in self.pool_keys:
+            if self.verifyDesirableAction(key):
+                actions.append(key)
+        return actions
 
     def verifyDesirableAction(self, action:int):
         op = self.op_name_dict[action]
@@ -41,8 +47,35 @@ class QASState():
             return False
         if len(op_qubit) == 2 and op_qubit[0] not in self.qubit_with_actions:
             return False
-
         return True
+
+    def takeAction(self, action:int):
+        new_qubit_with_actions = deepcopy(self.qubit_with_actions)
+        op = self.op_name_dict[action]
+        assert len(op.keys()) == 1  # in case some weird things happen
+        op_name = list(op.keys())[0]
+        op_qubit = op[op_name]
+        new_qubit_with_actions.add(op_qubit[-1])
+        new_path = self.current_k + [action]
+        new_state = QASState(current_k=new_path, op_pool=self.pool_obj, maxDepth=self.max_depth,
+                             qubit_with_actions=new_qubit_with_actions)
+        new_state.state = action
+        return new_state
+
+    def isTerminal(self):
+        return self.current_depth>=self.max_depth
+
+    def getReward(self):
+        return 0.
+
+    def __repr__(self):
+        disp = ""
+        for i, p in enumerate(self.current_k):
+            disp = disp + "OpNo: {}, OpKey: {}, OpName: {}".format(i, p, self.op_name_dict[p])
+        return disp
+
+
+
 
 
 
