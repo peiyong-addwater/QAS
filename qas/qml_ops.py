37f0dc4ddf68fa90253ec8aeaf515faecd26d410
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2021-09-22 05:10:22 +1000

    update

--- qas/qml_ops.py
+++ qas/qml_ops.py
@@ -90,3 +90,83 @@ SUPPORTED_OPS_DICT = {
     'IsingZZ':IsingZZ
 }
 
+SUPPORTED_OPS_NAME = set(SUPPORTED_OPS_DICT.keys())
+
+from .ops import Pool, QuantumGate
+
+class QMLGate(QuantumGate):
+
+    def __init__(self, name:str, pos:List[int], param:Optional[Sequence]=None):
+        assert name in SUPPORTED_OPS_NAME
+        op_constructor = SUPPORTED_OPS_DICT[name]
+        self.num_params = op_constructor.base_name
+        self.num_wires = op_constructor.num_wires
+        if param is not None:
+            assert len(param) == self.num_params
+        assert len(pos) == self.num_wires
+        self.wires = pos
+        self.name = name
+        self.op = op_constructor(*param, wires=self.wires) if param is not None else op_constructor(wires=self.wires)
+        self.param = param if param is not None else None
+
+    def get_op(self):
+        return self.op
+
+    def get_qreg_pos(self):
+        return list(self.wires)
+
+    def __str__(self):
+        disp = "{}, {}, {}".format(self.name, self.wires, self.param)
+        return disp
+
+class QMLPool(Pool):
+    def __init__(self, num_qubits: int, single_qubit_op_generator: List[AnyStr],
+                 two_qubit_op_generator: List[AnyStr],
+                 complete_undirected_graph: bool = True, two_qubit_gate_map: Optional[List[Tuple[int, int]]] = None):
+        super(QMLPool, self).__init__()
+        for c in single_qubit_op_generator:
+            op_constructor = SUPPORTED_OPS_DICT[c]
+            assert op_constructor.num_wires == 1
+        for c in two_qubit_op_generator:
+            op_constructor = SUPPORTED_OPS_DICT[c]
+            assert op_constructor.num_wires == 2
+        if two_qubit_gate_map is not None:
+            assert complete_undirected_graph is False
+            self.coupling = two_qubit_gate_map
+            for c in self.coupling:
+                assert len(c) == 2
+                assert c[0] != c[1]
+                assert c[0] >= 0
+                assert c[0] < num_qubits
+                assert c[1] >= 0
+                assert c[1] < num_qubits
+        else:
+            self.coupling = []
+            for i in range(num_qubits):
+                for j in range(num_qubits):
+                    if i != j:
+                        self.coupling.append((i, j))
+
+        self.num_qubits = num_qubits
+        self.single_qubit_gates = single_qubit_op_generator
+        self.two_qubit_gates = two_qubit_op_generator
+        self.pool = {}
+        pool_key = 0
+        for i in range(num_qubits):
+            for c in single_qubit_op_generator:
+                self.pool[pool_key] = {c: (i,)}
+                pool_key = pool_key + 1
+        for couple_direction in self.coupling:
+            for c in two_qubit_op_generator:
+                self.pool[pool_key] = {c: couple_direction}
+                pool_key = pool_key + 1
+
+    def __getitem__(self, key):
+        return self.pool[key]
+
+    def __str__(self):
+        return json.dumps(self.pool)
+
+    def __len__(self):
+        return len(self.pool.keys())
+
