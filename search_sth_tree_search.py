f92f82ce9012232c525c546332bdd84bec170a35
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2021-09-23 15:42:51 +1000

    update

--- search_sth_tree_search.py
+++ search_sth_tree_search.py
@@ -1,4 +1,4 @@
-from qas.mcts import search, TreeNode
+from qas.mcts import search, TreeNode, circuitModelTuning
 from qas.qml_ops import QMLPool
 from qas.qml_models import ToffoliQMLNoiseless
 import json
@@ -65,7 +65,7 @@ if __name__ == "__main__":
         num_iterations=500,
         num_warmup_iterations=50,
         super_circ_train_optimizer=optax.adam,
-        super_circ_train_gradient_noise_factor=1/100,
+        super_circ_train_gradient_noise_factor=1/50,
         super_circ_train_lr=0.1,
         penalty_function=penalty_func,
         arc_batchsize=200,
@@ -75,12 +75,37 @@ if __name__ == "__main__":
         prune_constant_min=0.5,
         max_visits_prune_threshold=100,
         min_num_children=5,
-        sampling_execute_rounds=100,
-        exploit_execute_rounds=100,
+        sampling_execute_rounds=150,
+        exploit_execute_rounds=50,
         sample_policy='local_optimal',
         exploit_policy='local_optimal'
     )
 
+
+    final_params, loss_list = circuitModelTuning(
+        initial_params=final_params,
+        model=model,
+        num_epochs=100,
+        k=final_best_arc,
+        op_pool=pool,
+        opt_callable=optax.adam,
+        lr=0.01,
+        grad_noise_factor=0,
+        verbose=1
+    )
+
+    res_dict = {
+        'task':task,
+        'pool':pool.pool,
+        'params':final_params,
+        'k':final_best_arc,
+        'op_list':model(p,c,l,final_best_arc,pool).toList(final_params),
+        'search_reward_list':reward_list,
+        'tuning_loss_list':loss_list
+    }
+
+    with open(filename, 'w') as f:
+        json.dump(res_dict, f, indent=4, cls=NpEncoder)
 
 
 
