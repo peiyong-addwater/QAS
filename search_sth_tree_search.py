4a8e76366f1ffc8d16cc523e43943ea2caaf5b92
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2021-09-25 19:31:51 +1000

    updates

--- search_sth_tree_search.py
+++ search_sth_tree_search.py
@@ -31,12 +31,12 @@ if __name__ == "__main__":
     model = ToffoliQMLNoiseless
     init_qubit_with_actions = {0,1,2}
     two_qubit_gate = ["CNOT"]
-    single_qubit_gate = ["PauliX", "SX", "RZ"]
+    single_qubit_gate = ["SX", "RZ"]
     #single_qubit_gate = ['U3']
-    control_map = [[0,1], [1,2]]
+    control_map = [[0,1], [1,2], [1,0], [2,1]]
     pool = QMLPool(3, single_qubit_gate, two_qubit_gate, complete_undirected_graph=False, two_qubit_gate_map=control_map)
     print(pool)
-    p = 35
+    p = 37
     l = 3
     c = len(pool)
 
@@ -48,8 +48,8 @@ if __name__ == "__main__":
             op_name = list(pool[op_index].keys())[0]
             if "CNOT" in op_name or  "CR" in op_name or "CY" in op_name or "CZ" in op_name or "CRot" in op_name:
                 cu3_count = cu3_count + 1
-        if cu3_count>=10:
-            return r - (cu3_count-11)
+        if cu3_count>=8:
+            return r - (cu3_count-8)
         return r
 
 
@@ -62,8 +62,8 @@ if __name__ == "__main__":
         target_circuit_depth=p,
         init_qubit_with_controls=init_qubit_with_actions,
         init_params=init_params,
-        num_iterations=200,
-        num_warmup_iterations=50,
+        num_iterations=400,
+        num_warmup_iterations=100,
         super_circ_train_optimizer=optax.adam,
         super_circ_train_gradient_noise_factor=1/50,
         super_circ_train_lr=0.1,
@@ -75,8 +75,8 @@ if __name__ == "__main__":
         prune_constant_min=0.5,
         max_visits_prune_threshold=100,
         min_num_children=5,
-        sampling_execute_rounds=10,
-        exploit_execute_rounds=10,
+        sampling_execute_rounds=20,
+        exploit_execute_rounds=5,
         sample_policy='local_optimal',
         exploit_policy='local_optimal'
     )
