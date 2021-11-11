3f236667b780e35c6522f584370c9aa735f888d1
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2021-11-11 23:57:08 +1100

    updates

--- search_toffoli_non_parameterized.py
+++ search_toffoli_non_parameterized.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     print(task)
     init_qubit_with_actions = {0, 1, 2}
     two_qubit_gate = ["CNOT"]
-    single_qubit_gate = ["T", 'Tdg','Hadamard']
+    single_qubit_gate = ["T", 'Tdg','Hadamard','S']
 
     # set a hard limit on the number of certain gate instead of using a penalty function
     gate_limit = {"CNOT": 6}
@@ -46,10 +46,10 @@ if __name__ == "__main__":
     pool = QMLPool(3, single_qubit_gate, two_qubit_gate, complete_undirected_graph=False,
                    two_qubit_gate_map=control_map)
     print(pool)
-    p = 15
+    p = 16
     l = 3
     c = len(pool)
-    ph_count_limit = p
+    ph_count_limit = 0
 
 
     # penalty function:
@@ -73,29 +73,29 @@ if __name__ == "__main__":
         target_circuit_depth=p,
         init_qubit_with_controls=init_qubit_with_actions,
         init_params=init_params,
-        num_iterations=50,
-        num_warmup_iterations=3,
+        num_iterations=500,
+        num_warmup_iterations=0,
         super_circ_train_optimizer=qml.AdamOptimizer,
         super_circ_train_gradient_noise_factor=0,
-        early_stop_threshold=0.90,
+        early_stop_threshold=0.95,
         early_stop_lookback_count=1,
         super_circ_train_lr=0.1,
         penalty_function=penalty_func,
         gate_limit_dict=gate_limit,
         warmup_arc_batchsize=2000,
-        search_arc_batchsize=200,
+        search_arc_batchsize=300,
         alpha_max=2,
         alpha_decay_rate=0.95,
-        prune_constant_max=0.99,
+        prune_constant_max=0.9,
         prune_constant_min=0.7,
         max_visits_prune_threshold=100,
         min_num_children=len(pool) // 2 + 1,
-        sampling_execute_rounds=c,
-        exploit_execute_rounds=c * 10,
+        sampling_execute_rounds=20,
+        exploit_execute_rounds=1000,
         cmab_sample_policy='local_optimal',
         cmab_exploit_policy='local_optimal',
         uct_sample_policy='local_optimal',
-        verbose=1,
+        verbose=2,
         state_class=state_class,
         search_reset=False
     )
