2ca4fce8b3fb83cb4e29098b7f7c129d760e582f
Peiyong Wang <peiyongw@student.unimelb.edu.au>
2021-12-10 10:18:51 +1100

    updates

---
+++ qas/qml_machine_learning_model.py
@@ -0,0 +1,32 @@
+from .models import ModelFromK
+from .qml_gate_ops import (
+    SUPPORTED_OPS_DICT,
+    SUPPORTED_OPS_NAME,
+    QMLPool,
+    QMLGate
+)
+import pennylane.numpy as pnp
+from pennylane.operation import Operation, AnyWires
+import numpy as np
+import pennylane as qml
+from typing import (
+    List,
+    Sequence,
+    Any,
+    Tuple,
+    Callable,
+    Iterator,
+    Optional,
+    Union,
+    Iterable,
+    Dict,
+    AnyStr
+)
+import qiskit
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import state_fidelity, DensityMatrix, Statevector
+from qiskit.providers.aer.backends import StatevectorSimulator, AerSimulator, QasmSimulator
+import qiskit.providers.aer.noise as noise
+import shutup
+shutup.please()
+
