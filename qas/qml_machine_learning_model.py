5b35a20ed13ae6365607a0f05be653f2ff7cdf01
Peiyong Wang <peiyongw@student.unimelb.edu.au>
2021-12-10 10:57:26 +1100

    updates

--- qas/qml_machine_learning_model.py
+++ qas/qml_machine_learning_model.py
@@ -27,6 +27,25 @@ from qiskit import QuantumCircuit
 from qiskit.quantum_info import state_fidelity, DensityMatrix, Statevector
 from qiskit.providers.aer.backends import StatevectorSimulator, AerSimulator, QasmSimulator
 import qiskit.providers.aer.noise as noise
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder
+from sklearn.decomposition import PCA
 import shutup
 shutup.please()
+
+# The data "challenge.npz" is from https://github.com/quantum-melb/MCDS-workshop/blob/main/day-four/data/challenge.npz
+# Load the Fashion-MNIST data in the folder "additional_data"
+DATA_PATH = './additional_data/challenge.npz'
+data = np.load(DATA_PATH)
+sample_train = data['sample_train']
+labels_train = data['labels_train']
+
+# Split train data
+sample_train, sample_val, labels_train, labels_val = train_test_split(
+    sample_train, labels_train, test_size=0.2, random_state=42)
+
+# Load test data
+sample_test = data['sample_test']
+labels_test = data['labels_test']
 
