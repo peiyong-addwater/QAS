348f4aa644b146ae8fb07678632b387d310a9737
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2022-02-08 22:12:41 +1100

    updates

--- search_toffoli_additional_data.py
+++ search_toffoli_additional_data.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     print(task)
     init_qubit_with_actions = {0, 1, 2}
     two_qubit_gate = ["CNOT"]
-    single_qubit_gate = ['Hadamard', 'S', 'T', 'Tdg','PlaceHolder']
+    single_qubit_gate = ['U3','PlaceHolder']
 
     # set a hard limit on the number of certain gate instead of using a penalty function
     gate_limit = {"CNOT": 6}
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     p = 16
     l = 3
     c = len(pool)
-    ph_count_limit = 0
+    ph_count_limit = 1
 
 
     # penalty function:
@@ -71,25 +71,25 @@ if __name__ == "__main__":
         target_circuit_depth=p,
         init_qubit_with_controls=init_qubit_with_actions,
         init_params=init_params,
-        num_iterations=50,
-        num_warmup_iterations=3,
+        num_iterations=500,
+        num_warmup_iterations=50,
         super_circ_train_optimizer=qml.AdamOptimizer,
         super_circ_train_gradient_noise_factor=0,
         early_stop_threshold=0.90,
         early_stop_lookback_count=1,
-        super_circ_train_lr=0.1,
+        super_circ_train_lr=0.3,
         penalty_function=penalty_func,
         gate_limit_dict=gate_limit,
         warmup_arc_batchsize=20,
         search_arc_batchsize=200,
         alpha_max=2,
         alpha_decay_rate=0.95,
-        prune_constant_max=0.99,
+        prune_constant_max=0.8,
         prune_constant_min=0.7,
-        max_visits_prune_threshold=100,
-        min_num_children=len(pool) // 2 + 1,
-        sampling_execute_rounds=4,
-        exploit_execute_rounds=c // 2+1,
+        max_visits_prune_threshold=20,
+        min_num_children=c//2+1,
+        sampling_execute_rounds=c,
+        exploit_execute_rounds=c*2,
         cmab_sample_policy='local_optimal',
         cmab_exploit_policy='local_optimal',
         uct_sample_policy='local_optimal',
