1f10a68cbedd12e7ed0a9d352a812a42b6e739eb
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2022-02-21 12:12:47 +1100

    updates

--- search_toffoli_additional_data.py
+++ search_toffoli_additional_data.py
@@ -36,8 +36,8 @@ if __name__ == "__main__":
     print(task)
     init_qubit_with_actions = {0, 1, 2}
     two_qubit_gate = ["CNOT"]
-    #single_qubit_gate = ['U3','PlaceHolder']
-    single_qubit_gate = ['Hadamard', 'S', 'T', 'Tdg', 'Sdg', 'PlaceHolder']
+    single_qubit_gate = ['U3','PlaceHolder']
+    #single_qubit_gate = ['Hadamard', 'S', 'T', 'Tdg', 'Sdg', 'PlaceHolder']
 
     # set a hard limit on the number of certain gate instead of using a penalty function
     gate_limit = {"CNOT": 6}
@@ -73,19 +73,19 @@ if __name__ == "__main__":
         init_qubit_with_controls=init_qubit_with_actions,
         init_params=init_params,
         num_iterations=500,
-        num_warmup_iterations=0,
+        num_warmup_iterations=5,
         super_circ_train_optimizer=qml.AdamOptimizer,
-        super_circ_train_gradient_noise_factor=0.0,
+        super_circ_train_gradient_noise_factor=0.01,
         early_stop_threshold=0.90,
         early_stop_lookback_count=1,
-        super_circ_train_lr=0.5,
+        super_circ_train_lr=0.1,
         penalty_function=penalty_func,
         gate_limit_dict=gate_limit,
-        warmup_arc_batchsize=10,
+        warmup_arc_batchsize=5,
         search_arc_batchsize=20,
-        alpha_max=1,
-        alpha_decay_rate=0.9,
-        prune_constant_max=0.8,
+        alpha_max=2,
+        alpha_decay_rate=0.99,
+        prune_constant_max=0.99,
         prune_constant_min=0.5,
         max_visits_prune_threshold=20,
         min_num_children=4,
@@ -94,9 +94,9 @@ if __name__ == "__main__":
         cmab_sample_policy='local_optimal',
         cmab_exploit_policy='local_optimal',
         uct_sample_policy='local_optimal',
-        verbose=2,
+        verbose=1,
         state_class=state_class,
-        search_reset=False
+        search_reset=True
     )
 
     final_params, loss_list = circuitModelTuning(
