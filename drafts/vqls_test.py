365527326531bea80bc60c10e6c21fb2d1905f31
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2022-04-05 20:13:44 +1000

    updates

--- drafts/vqls_test.py
+++ drafts/vqls_test.py
@@ -19,7 +19,7 @@ J = 0.1
 zeta = 5
 eta = 1
 
-n_layers = 6
+n_layers = 5
 n_qubits = 4  # Number of system qubits.
 n_shots = 10 ** 6  # Number of quantum measurements.
 tot_qubits = n_qubits + 1  # Addition of an ancillary qubit.
