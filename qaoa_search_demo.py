5d3e1c99ae436135f3b8690f89eaff4f854e2aa0
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2022-04-06 02:05:37 +1000

    updates

--- qaoa_search_demo.py
+++ qaoa_search_demo.py
@@ -96,7 +96,7 @@ if __name__ == "__main__":
         max_visits_prune_threshold=10,
         min_num_children=6,
         sampling_execute_rounds=100,
-        exploit_execute_rounds=50,
+        exploit_execute_rounds=500,
         cmab_sample_policy='local_optimal',
         cmab_exploit_policy='local_optimal',
         verbose=2,
