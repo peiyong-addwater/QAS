b3dda5c8e8e3e1db065779779dffeff7689ab7ac
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2022-04-06 20:31:57 +1000

    updates

--- vqls_search.py
+++ vqls_search.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
     # set a hard limit on the number of certain gate instead of using a penalty function
     pool = QMLPool(num_qubits, single_qubit_gate, two_qubit_gate, complete_undirected_graph=True)#, two_qubit_gate_map=connection_graph)
     print(pool)
-    p = 20
+    p = 10
     l = 3
     c = len(pool)
     gate_limit = {"CNOT": p//2+5}
@@ -64,7 +64,7 @@ if __name__ == "__main__":
         num_warmup_iterations=5,
         super_circ_train_optimizer=qml.AdamOptimizer,
         super_circ_train_gradient_noise_factor=0,
-        early_stop_threshold=0.95,
+        early_stop_threshold=0.98,
         early_stop_lookback_count=1,
         super_circ_train_lr=0.1,
         penalty_function=None,
