6538970beb59fdbf68951f6e3bf75e8f2a24e437
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2022-04-12 21:20:34 +1000

    updates

--- qas/qml_models.py
+++ qas/qml_models.py
@@ -2684,7 +2684,7 @@ class VQLSDemo5Q(ModelFromK):
     def getReward(self, super_circ_params):
         loss = self.getLoss(super_circ_params)
         # scale the reward
-        return np.exp(-10*np.abs(loss))
+        return np.exp(-20*np.abs(loss))
 
     def getGradient(self, super_circ_params:Union[np.ndarray, pnp.ndarray, Sequence]):
         assert super_circ_params.shape[0] == self.p
