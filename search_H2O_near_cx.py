98708dead7b471634c0284aa1f7e98122eb56d5d
Peiyong Wang <peiyongw@student.unimelb.edu.au>
2022-05-04 17:27:02 +1000

    updates

--- search_H2O_near_cx.py
+++ search_H2O_near_cx.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
         num_warmup_iterations=2,
         super_circ_train_optimizer=qml.AdamOptimizer,
         super_circ_train_gradient_noise_factor=0.0,
-        early_stop_threshold=75,
+        early_stop_threshold=-target_energy,
         early_stop_lookback_count=1,
         super_circ_train_lr=1,
         penalty_function=penalty_func,
