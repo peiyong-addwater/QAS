3bf7a56d0fb4b8b6507b0ab10a8ea0f0171eea73
Peiyong Wang <peiyongw@student.unimelb.edu.au>
2022-05-04 15:22:33 +1000

    updates

--- search_H2O_near_cx.py
+++ search_H2O_near_cx.py
@@ -66,8 +66,8 @@ if __name__ == "__main__":
     p = 50
     l = 3
     c = len(pool)
-    ph_count_limit = p
-    gate_limit = {"CNOT": p//3}
+    ph_count_limit = 0
+    gate_limit = {"CNOT": p//2}
 
 
     # penalty function:
@@ -79,7 +79,7 @@ if __name__ == "__main__":
             if op_name == 'PlaceHolder':
                 ph_count = ph_count + 1
         if ph_count >= ph_count_limit:
-            return r - (ph_count - ph_count_limit) / 1
+            return r - (ph_count - ph_count_limit) * 10
         return r
 
 
@@ -92,7 +92,7 @@ if __name__ == "__main__":
         init_qubit_with_controls=init_qubit_with_actions,
         init_params=init_params,
         num_iterations=50,
-        num_warmup_iterations=3,
+        num_warmup_iterations=2,
         super_circ_train_optimizer=qml.AdamOptimizer,
         super_circ_train_gradient_noise_factor=0.0,
         early_stop_threshold=74.9,
@@ -100,16 +100,16 @@ if __name__ == "__main__":
         super_circ_train_lr=1,
         penalty_function=penalty_func,
         gate_limit_dict=gate_limit,
-        warmup_arc_batchsize=500,
-        search_arc_batchsize=25,
+        warmup_arc_batchsize=100,
+        search_arc_batchsize=50,
         alpha_max=2,
         alpha_decay_rate=0.99,
         prune_constant_max=0.9,
         prune_constant_min=0.8,
         max_visits_prune_threshold=10,
         min_num_children=5,
-        sampling_execute_rounds=3,
-        exploit_execute_rounds=5,
+        sampling_execute_rounds=10,
+        exploit_execute_rounds=20,
         cmab_sample_policy='local_optimal',
         cmab_exploit_policy='local_optimal',
         uct_sample_policy='local_optimal',
@@ -121,7 +121,7 @@ if __name__ == "__main__":
     final_params, loss_list = circuitModelTuning(
         initial_params=final_params,
         model=model,
-        num_epochs=400,
+        num_epochs=100,
         k=final_best_arc,
         op_pool=pool,
         opt_callable=qml.AdamOptimizer,
