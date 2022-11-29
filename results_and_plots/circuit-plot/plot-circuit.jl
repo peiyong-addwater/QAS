bb59a3b0f91ac5885f97014e9171cc4d5dbc4167
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2022-11-29 17:45:12 +1100

    updates

--- results_and_plots/circuit-plot/plot-circuit.jl
+++ results_and_plots/circuit-plot/plot-circuit.jl
@@ -31,6 +31,39 @@ circuit_h2 = [
              ]
 savecircuit(circuit_h2, "h2_circuit.pdf")
 
+circuit_h2o = [
+                U("Rot(\\phi_0, \\theta_0, \\omega_0)",3),
+                U("Rot(\\phi_1, \\theta_1, \\omega_1)",5),
+                U("Rot(\\phi_2, \\theta_2, \\omega_2)",8),
+                CNOT(3,2),CNOT(5,6),CNOT(8,7),
+                U("Rot(\\phi_3, \\theta_3, \\omega_3)",2),
+                U("Rot(\\phi_4, \\theta_4, \\omega_4)",3),
+                CNOT(6,5),
+                U("Rot(\\phi_5, \\theta_5, \\omega_5)",8),
+                CNOT(2,1),CNOT(3,4),
+                U("Rot(\\phi_6, \\theta_6, \\omega_6)",5),
+                CNOT(8,7),
+                U("Rot(\\phi_7, \\theta_7, \\omega_7)",1),
+                CNOT(3,2),
+                U("Rot(\\phi_8, \\theta_8, \\omega_8)",4),
+                U("Rot(\\phi_9, \\theta_9, \\omega_9)",7),
+                CNOT(2,1),CNOT(8,7),
+                U("Rot(\\phi_{10}, \\theta_{10}, \\omega_{10})",1),
+                U("Rot(\\phi_{11}, \\theta_{11}, \\omega_{11})",2),
+                CNOT(7,6),CNOT(6,5),
+                U("Rot(\\phi_{12}, \\theta_{12}, \\omega_{12})",7),
+                U("Rot(\\phi_{13}, \\theta_{13}, \\omega_{13})",6),
+                CNOT(8,7),CNOT(5,6),CNOT(7,8),
+                U("Rot(\\phi_{14}, \\theta_{14}, \\omega_{14})",6),
+                U("Rot(\\phi_{15}, \\theta_{15}, \\omega_{15})",7),
+                U("Rot(\\phi_{16}, \\theta_{16}, \\omega_{16})",8),
+                CNOT(5,6),CNOT(4,5),
+                U("Rot(\\phi_{17}, \\theta_{17}, \\omega_{17})",6),
+                CNOT(3,4),
+                U("Rot(\\phi_{18}, \\theta_{18}, \\omega_{18})",5),
+                U("Rot(\\phi_{19}, \\theta_{19}, \\omega_{19})",3)
+              ]
 
+savecircuit(circuit_h2o, "h2o_circuit.pdf")
 
 
