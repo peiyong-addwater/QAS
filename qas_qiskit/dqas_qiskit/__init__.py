089ee6166bd14ffa2f38e6213ae95666c1951447
peiyong-addwater <peiyongw@student.unimelb.edu.au>
2023-02-08 05:56:23 +1100

    clean up

--- qas_qiskit/dqas_qiskit/__init__.py
+++
@@ -1,9 +0,0 @@
-__version__ = "0.0.1"
-__author__ = "Peiyong Wang"
-
-import numpy
-numpy.set_printoptions(precision=4,linewidth=1000)
-
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='3'
-os.environ['XLA_PYTHON_CLIENT_PREALLOCATE']='false'
